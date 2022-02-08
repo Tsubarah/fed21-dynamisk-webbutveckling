373f436ead61893468824a0f9d078686cf3aef51
Benjamin Tsubarah <benjamin@tsubarah.com>
2022-02-08 20:55:37 +0100

    Added connect-file

---
+++ 03-db/connect.js
@@ -0,0 +1,41 @@
+const mysql = require('mysql');
+
+let con = mysql.createConnection(
+    {
+        host: 'localhost',
+        port: '3306',
+        user: 'root',
+        password: 'root',
+        database: 'Pokemon'
+    }
+);
+
+// Connect to server
+con.connect(function(err) {
+    //console.log(err.message);
+    if (err) throw err;
+
+    console.log('Connected to database');
+
+    // Get id, name, hp from mySql
+    let sql = 'SELECT id, name, hp FROM pokemonCards';
+    sql = sql + ' WHERE id = 3';
+
+    // Ask a question to database with .query
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log('We have a result');
+        // console.log(result);
+        // result.forEach(row => {
+        //     console.log('Pokemon has the name ' + row.name + ' with hp ' + row.hp);
+        // });
+
+        for (let i = 0; i < result.length; i++) {
+            row = result[i];
+            console.log('Pokemon has the name ' + row.name + ' with hp ' + row.hp);
+        }
+
+        con.end();
+    });
+});
+
