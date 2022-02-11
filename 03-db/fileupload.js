f2d556c6df2dc0b01ed5d0cdf7ba33bae60328bf
Tsubarah <Tsubarah@hotmail.se>
2022-02-11 11:40:59 +0100

    Created functions to upload files to the uploads folder

--- 03-db/fileupload.js
+++ 03-db/fileupload.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express();
+const multer = require('multer'); // Load multer
 
 app.use(express.static('static'));
 
+// const urlencoded = express.urlencoded({ extended: false }); <-- For encoded URLs
+// const jsonencoder = express.json(); <-- For json
 
+// Save file on the diskStorage
+const storageObject = multer.diskStorage(
+  {
+    destination: (req, file, cb) => {
+      cb(null, 'uploads');
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + '-' + file.originalname); // Save file with unique name by adding the current date
+    }
+  }
+);
+
+// Start multer
+const multipartDataEncoder = multer({ storage: storageObject });
+
+// Use multer for a single file
+app.post('/upload', multipartDataEncoder.single('myFile'), (req, res) => {
+  console.log(req.file);
+  res.send(req.file);
+});
 
 app.listen(3000, () => {
 	console.log(`🥳 Yay, server started at http://localhost:3000`);
