d437537b80d98bc0bb5653adc57d384647ea9b1d
Benjamin Tsubarah <benjamin@tsubarah.com>
2022-03-01 10:41:53 +0100

    changed register_controller to auth_controller

--- 06-books/routes/index.js
+++ 06-books/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middlewares/auth');
+const authController = require('../controllers/auth_controller');
+const userValidationRules = require('../validation/user');
 
 /* GET / */
 router.get('/', (req, res, next) => {
@@ -10,6 +12,8 @@ router.get('/', (req, res, next) => {
 router.use('/authors', require('./authors'));
 router.use('/books', require('./books'));
 router.use('/profile', auth.basic, require('./profile'));
-router.use('/users', require('./users'));
+
+// register a new user
+router.post('/register', userValidationRules.createRules, authController.register);
 
 module.exports = router;
